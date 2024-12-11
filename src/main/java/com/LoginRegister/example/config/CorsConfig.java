355ec28b0e09e14608328f39fd02117b26b3cd50
Bindu sagar <145993364+2200032348@users.noreply.github.com>
2024-12-12 01:56:30 +0530

    Update CorsConfig.java

--- src/main/java/com/LoginRegister/example/config/CorsConfig.java
+++ src/main/java/com/LoginRegister/example/config/CorsConfig.java
@@ -13,7 +13,6 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
