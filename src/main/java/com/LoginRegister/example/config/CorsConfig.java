c8bcdbbf684cb21eee75a9109f221cc1d8243599
Bindu sagar <145993364+2200032348@users.noreply.github.com>
2024-12-12 01:37:34 +0530

    Update CorsConfig.java

--- src/main/java/com/LoginRegister/example/config/CorsConfig.java
+++ src/main/java/com/LoginRegister/example/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://genderviolance-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*")
                         .allowCredentials(true);
